065d67d9321e4d824bdb567ea3c699c9fa17a813
gc48 <gc48@uw.edu>
2021-04-22 11:38:40 -0700

    fontset added, cycle broken into fetch decode execute

--- chip8.cpp
+++ chip8.cpp
@@ -15,9 +15,14 @@ void chip8::initialize() {
   //reset timers
 }
 
-void chip8::emulateCycle() {
-  OP = (memory[PC] << 8) | memory[PC+1];
+
+void chip8::fetch() {
   
+  OP = (memory[PC] << 8) | memory[PC+1];
+}
+
+void chip8::decode_execute() {
+
   switch(OP & 0xF000) { // leftmost 4 bits used for opcode interpretation
     case 0xA000:
       I = OP & 0x0FFF;
@@ -30,10 +35,19 @@ void chip8::emulateCycle() {
 
   } 
 
+}
+
+void chip8::update_timers() {
   delay_timer = (delay_timer > 0) ? delay_timer-1 : 0;
   if(sount_timer == 1) {
     printf("BEEP!\n");
     //put a sound here!
   }
   sound_timer = (sound_timer > 0) ? sound_timer-1 : 0;
+}
+
+void chip8::emulateCycle() {
+  fetch();
+  decode_execute();
+  update_timers(); 
 }
